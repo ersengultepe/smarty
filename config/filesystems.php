d2448203ecd9bd97808db7758a1ac5c4c3a98816
Ersen Gültepe <ersengultepe@gmail.com>
2023-01-02 00:53:56 +0300

    filesystems den resim kaydetme adresi değiştirildi 3

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,10 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-//            'root' => storage_path('app/public'),
-            'root' => 'storage/app/public',
-//            'url' => env('APP_URL').'/storage',
-            'url' => env('APP_URL', 'http://www.physmart.com.tr/').'/storage',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
