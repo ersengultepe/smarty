0e91262f54a037ff53413c7d2c1959502dee7f1b
Ersen Gültepe <ersengultepe@gmail.com>
2023-01-02 01:08:33 +0300

    filesystems den resim kaydetme adresi değiştirildi 5

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://www.physmart.com.tr'),
+    'url' => env('APP_URL', 'http://www.physmart.com.tr/public'),
 
     'asset_url' => env('ASSET_URL', null),
 
