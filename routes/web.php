1d159798ab6aa6f2b9c60119704c55351c108489
Ersen Gültepe <ersengultepe@gmail.com>
2023-01-04 21:47:25 +0300

    controllerlar eklendi 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use TCG\Voyager\Voyager;
